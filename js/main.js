30f6a6476ad67905d833ba8e795a66ecebf8fe54
vasilek333 <59596441+vasilek333@users.noreply.github.com>
2021-03-19 09:03:46 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+$('#app').vide({
+  mp4: '../video/video.mp4',
+  webm: '../video/video.webm',
+  ogv: '../video/video.ogv',
+  poster: '../img/cover_image.jpg'
+});
+
+});
